2cd4b7ff1fe783b3bf4fc1c8d7494ece85cffde6
Remy Zulauff <remy.zulauff@gmail.com>
2023-07-02 17:04:56 +0200

    feat(root.go): add support for reading configuration from a YAML file and environment variables

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,24 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "godoorpc",
 	Short: "Odoo RPC CLI tool",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `GodooRPC allow you to communicate with Odoo ERP through it's external API.
+	
+Both XML and JSON RPC protocols are supported.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("This is a demo")
+	},
 }
 
 func Execute() {
@@ -26,4 +30,33 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.godoorpc.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".godoorpc" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".godoorpc")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
